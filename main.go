1b038404c871d3bd1744ce072fc7619b28481d06
Ruslan_L <rus059111@gmailcom>
2023-01-23 19:49:39 +0600

    решение

--- main.go
+++ main.go
@@ -2,6 +2,29 @@ package main
 
 //Задание 4. Предыдущее занятие на if
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var a, b, c int = 0, 0, 0
+	limit1 := 10
+	limit2 := 100
+	limit3 := 1000
+
+	for {
+		fmt.Println(a, b, c)
+		if c == limit3 {
+			break
+		}
+		c += 1
+		if b == limit2 {
+			continue
+		}
+		b += 1
+		if a == limit1 {
+			continue
+		}
+		a += 1
+	}
 }
